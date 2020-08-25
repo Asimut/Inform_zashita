922393d82bf2ecabcb2b41b15a443fa9cc13df43
Asimut <asimut007@gmail.com>
2020-08-25 15:58:54 +0300

    Oleg

---
+++ story_content/user.js
@@ -0,0 +1,32 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6AkrLRJwRpR":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var storylinePlayer = GetPlayer();
+var newIncomingText = storylinePlayer.GetVar("newText");
+var newOutgoingText = "";
+var myArray = newIncomingText.split("");
+var arrayLength = myArray.length;
+
+function typeText() {
+  newOutgoingText = newOutgoingText + myArray[0];
+  myArray.shift();
+  storylinePlayer.SetVar("displayText",newOutgoingText);
+  arrayLength = myArray.length;
+  if (arrayLength == 0) {
+     clearInterval(myInterval);
+     }
+  }
+
+// A letter is typed every 100ms, adjust the typing speed by changing the 100 number below
+var myInterval = setInterval(typeText, 100); 
+}
+
